6c41f1d3d3aff9be45f0944aa85c6083f8288d32
Tessapj99 <paul.mary11299@gmail.com>
2021-09-06 12:44:38 +0530

    added side bar cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import SideBar from './components/SideBar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div> 
+      <SideBar />
+    
+      
     </div>
   );
 }
